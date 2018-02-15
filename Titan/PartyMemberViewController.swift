075f49b3ebd7b9f1bd9144a9ea891d823bb6b1de
Tim <tjgambs@gmail.com>
2018-02-15 15:55:22 -0600

    Search result buttons added

--- Titan/PartyMemberViewController.swift
+++ Titan/PartyMemberViewController.swift
@@ -14,6 +14,10 @@ class PartyMemberViewController: UIViewController {
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var searchBox: UITextField!
 
+    @IBOutlet weak var searchResults: UIStackView!
+    
+    var searchResultButtons: [UIButton] = []
+    
     struct Response: Codable {
         let data: [Song]
         let meta: MetaVariable
@@ -42,15 +46,36 @@ class PartyMemberViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        print(sender.titleLabel!.text ?? "")
+    }
+    
+    func createButton(_ song: Song) -> UIButton {
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 15))
+        button.backgroundColor = .green
+        button.setTitle(song.name + " - " + song.artist, for: [])
+        button.addTarget(self, action: #selector(self.buttonAction), for: .touchUpInside)
+        return button
+    }
+    
     @IBAction func searchSong(_ sender: UIButton) {
         let text = searchBox.text ?? ""
         titanAPI.searchSong(text) { (responseDict) in
             do {
                 let jsonDecoder = JSONDecoder()
                 let response = try jsonDecoder.decode(Response.self, from: responseDict)
+                DispatchQueue.main.async {
+                    for b in self.searchResultButtons {
+                        self.searchResults.removeArrangedSubview(b)
+                    }
+                }
                 for song in response.data {
-                    //TODO - Create buttons based on the Song object.
-                    print(song.name)
+                    DispatchQueue.main.async {
+                        let button = self.createButton(song)
+                        self.searchResultButtons.append(button)
+                        self.searchResults.addArrangedSubview(button)
+                    }
                 }
             } catch {}
            

708ad8b1733346a7e14f727887b13f9a6bbd4a06
Tim <tjgambs@gmail.com>
2018-02-15 14:16:57 -0600

    Clean up

--- Titan/SpotifyAuthViewController.swift
+++ Titan/SpotifyAuthViewController.swift
@@ -10,40 +10,24 @@ import UIKit
 import SafariServices
 import AVFoundation
 
-
-
 class SpotifyAuthViewController: UIViewController, SPTAudioStreamingPlaybackDelegate, SPTAudioStreamingDelegate {
-    
-    //--------------------------------------
-    // MARK: Variables
-    //--------------------------------------
-    
-    // Variables
+
     var auth = SPTAuth.defaultInstance()!
     var session:SPTSession!
-    
-    // Initialzed in either updateAfterFirstLogin: (if first time login) or in viewDidLoad (when there is a check for a session object in User Defaults
     var player: SPTAudioStreamingController?
     var loginUrl: URL?
-    
-    
-    //--------------------------------------
-    // MARK: Outlets
-    //--------------------------------------
-    
+
     @IBOutlet weak var loginButton: UIButton!
-    
-    
-    //--------------------------------------
-    // MARK: Functions
-    //--------------------------------------
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        setup()
+        let redirectURL = "Titan://returnAfterLogin"
+        auth.redirectURL     = URL(string: redirectURL)
+        auth.clientID        = "362fc5bdd0d5461499ccf6e4e7483800"
+        auth.requestedScopes = [SPTAuthStreamingScope, SPTAuthPlaylistReadPrivateScope, SPTAuthPlaylistModifyPublicScope, SPTAuthPlaylistModifyPrivateScope]
+        loginUrl = auth.spotifyWebAuthenticationURL()
         NotificationCenter.default.addObserver(self, selector: #selector(SpotifyAuthViewController.updateAfterFirstLogin), name: NSNotification.Name(rawValue: "loginSuccessfull"), object: nil)
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -51,69 +35,36 @@ class SpotifyAuthViewController: UIViewController, SPTAudioStreamingPlaybackDele
         // Dispose of any resources that can be recreated.
     }
     
-    func setup () {
-        // insert redirect your url and client ID below
-        let redirectURL = "Titan://returnAfterLogin" // put your redirect URL here
-        auth.redirectURL     = URL(string: redirectURL)
-        auth.clientID        = "362fc5bdd0d5461499ccf6e4e7483800"
-        auth.requestedScopes = [SPTAuthStreamingScope, SPTAuthPlaylistReadPrivateScope, SPTAuthPlaylistModifyPublicScope, SPTAuthPlaylistModifyPrivateScope]
-        loginUrl = auth.spotifyWebAuthenticationURL()
-        
-    }
-    
     func initializePlayer(authSession:SPTSession){
         if self.player == nil {
-            
-            
             self.player = SPTAudioStreamingController.sharedInstance()
             self.player!.playbackDelegate = self
             self.player!.delegate = self
             try! player?.start(withClientId: auth.clientID)
             self.player!.login(withAccessToken: authSession.accessToken)
-            
         }
-        
     }
     
     @objc func updateAfterFirstLogin () {
-        
         loginButton.isHidden = true
         let userDefaults = UserDefaults.standard
-        
         if let sessionObj:AnyObject = userDefaults.object(forKey: "SpotifySession") as AnyObject? {
-            
             let sessionDataObj = sessionObj as! Data
             let firstTimeSession = NSKeyedUnarchiver.unarchiveObject(with: sessionDataObj) as! SPTSession
-            
             self.session = firstTimeSession
             initializePlayer(authSession: session)
-            self.loginButton.isHidden = true
-            // self.loadingLabel.isHidden = false
-            
         }
-        
     }
     
     func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
-        // after a user authenticates a session, the SPTAudioStreamingController is then initialized and this method called
-        print("logged in")
         self.player?.playSpotifyURI("spotify:track:58s6EuEYJdlb0kO7awm3Vp", startingWith: 0, startingWithPosition: 0, callback: { (error) in
-            if (error != nil) {
-                print("playing!")
-            }
+            
         })
-    self.navigationController!.navigationBar.isHidden = true;
-        
+        self.navigationController!.navigationBar.isHidden = true;
     }
     
     
     @IBAction func loginButtonPressed(_ sender: Any) {
         UIApplication.shared.open(loginUrl!, options: [:], completionHandler: nil)
-        if auth.canHandle(auth.redirectURL) {
-            // To do - build in error handling
-        }
     }
-    
-    
-    
 }

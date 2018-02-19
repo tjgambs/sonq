3962fdc3bd2fca2ce66aabe47714085b468fb954
Tim <tjgambs@gmail.com>
2018-02-19 00:37:33 -0600

    Modified for the cloud

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = "postgres://titan:titancsc371@35.188.136.193:5432/titan"
+SQLALCHEMY_DATABASE_URI = "postgres://postgres:Jbu6xi8jmf2oy00g@35.188.8.242:5432/titan"
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_POOL_TIMEOUT	= 10

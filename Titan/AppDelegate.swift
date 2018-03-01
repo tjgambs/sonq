7d9f4ea1fe90ac13e919712c2b21b5abc9c7bc2a
Tim <tjgambs@gmail.com>
2018-02-28 22:11:24 -0600

    Only disable current music if they are the host

--- Titan/AppDelegate.swift
+++ Titan/AppDelegate.swift
@@ -17,11 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Audio Set-up. Set audio to "Playback" mode.
-        let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setCategory(AVAudioSessionCategoryPlayback)
-        } catch {
-            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        
+        if Globals.partyDeviceId != nil {
+            let audioSession = AVAudioSession.sharedInstance()
+            do {
+                try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+            } catch {
+                print("Setting category to AVAudioSessionCategoryPlayback failed.")
+            }
         }
         // If the user has not logged in yet, go to login screen
         if !LoginManager.shared.isLogged {

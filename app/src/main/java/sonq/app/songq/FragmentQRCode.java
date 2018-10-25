dfae09f789de036687e6e45ffb7804b69f18370a
Cody Dietrich <cody.dietrich156@gmail.com>
2018-10-24 21:07:54 -0500

    Change some colors and add qr code
    Feel free to change colors

--- app/src/main/java/sonq/app/songq/FragmentQRCode.java
+++ app/src/main/java/sonq/app/songq/FragmentQRCode.java
@@ -1,18 +1,37 @@
 package sonq.app.songq;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import net.glxn.qrgen.android.QRCode;
 
 public class FragmentQRCode extends Fragment {
+
+    private ImageView imageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.qr_code_view, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Bitmap bitmap = QRCode.from(PartyActivity.PARTY_ID)
+                .withSize(250, 250)
+                .bitmap();
+
+        imageView = getView().findViewById(R.id.QRimageView);
+        imageView.setImageBitmap(bitmap);
     }
 
 }

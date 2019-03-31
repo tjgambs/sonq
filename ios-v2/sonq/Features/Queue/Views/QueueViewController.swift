434b982febb190bc3de7f816bc293040e7bb9735
Tim <tjgambs@gmail.com>
2019-03-31 03:54:14 -0500

    Added some emptyyyyyyyyyy text

--- ios-v2/sonq/Features/Queue/Views/QueueViewController.swift
+++ ios-v2/sonq/Features/Queue/Views/QueueViewController.swift
@@ -9,22 +9,12 @@
 import UIKit
 
 class QueueViewController: ViewController {
-
+    
+    @IBOutlet weak var emptyText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        emptyText.text = "The Queue is empty. Search for songs to add to the queue."
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
